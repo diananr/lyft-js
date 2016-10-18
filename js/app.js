fd7e7177b16cad59a3f9639b57ed2571fa5296fc
diananr <navarroruiz.diana>
2016-10-17 21:28:13 -0500

    agregando algunas vistas y validaciones

--- js/app.js
+++ js/app.js
@@ -1,29 +1,66 @@
-// var digCod = function(e){
-// 	var dig1 = $(e.target);
-// 	var dig2 = dig1.parent().next().children();
-// 	var dig3 = dig2.parent().next().children();
+var valDatos = function(){
+	if ($("#nomb").val().trim().length == 0 || $("#apell").val().trim().length == 0 || $("#email").val().trim().length == 0) {
+		alert("Complete todos los espacios");
+	}
+	if ($("#nomb").val().trim().length > 3 && $("#nomb").val().trim().length < 21) {
+		return true;
+	}
+	else
+		alert("Ingrese bien su nombre");
 
-// 	var long = dig1.val().length;
-// 	if (long == 1){
-// 		dig2.focus();
-// 		var long2 = dig2.val().length;
-// 		if(long2 == 1){
-// 			dig3.focus();
-// 		}
-// 	}
-// }
-// var compararCodigo = function(){
-// 	var obtenCod = localStorage.getItem("guardarCod");
-// 	for (var i = 3; i < 6; i++) {
-// 		for (var j = 0; j < 3; j++) {
-// 			if (obtenCod[i] != ($("input").get(j).val())){
-// 				alert("Ingrese su código por favor");
-// 			}
-// 		}
-// 	}
-// }
-var copNum = function(){
+	if ($("#apell").val().trim().length > 3 && $("#apell").val().trim().length < 21) {
+		return true;
+	}
+	else
+		alert("Ingrese bien su apellido");
+
+	var regexEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    if (!regexEmail.test($("#email").val())){
+        alert("Ingresa bien su email");
+    }
+    if ($("#email").val().trim().length > 6 && $("#email").val().trim().length < 51) {
+		return true;
+	}
+	else
+		alert("Ingrese bien su email");
+}
+var cargaPag = function(){
+	$("#numeros").keydown(validar);
+	// $num.keyup(comprobar);
+	$("#numeros").on("input", function(e){
+		validar(e);
+		comprobar(e);
+	});
 	$("#celu").text(localStorage.getItem("celular")); 
+	$("#sgt2").click(compararCodigo);
+	$("#dig1").focus();
+	$(".cnum").keyup(saltaFocus);
+	$("#sgt3").click(valDatos);
+	if (navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(todoBien, hayError);
+	}
+}
+
+$(document).ready(cargaPag);
+
+var validar = function(e){
+	var tecla = e.keyCode;
+	if((tecla >= 48 && tecla <= 57) || tecla == 8){
+		return true;
+	}
+	else
+		return false;
+}
+var comprobar = function(e){
+	var caracteres = $(e.target).val().length;
+	if (caracteres == 9){
+		$("#next").attr("href", "verify.html");
+		$("#next").click(generarCod);
+	}
+	else{
+		$("#next").removeAttr("href");
+		$("#next").unbind("click");
+	}
 }
 var generarCod = function(){
 	var codigo = "LAB";
@@ -38,37 +75,56 @@ var generarCod = function(){
 	var obtNum = $("#numeros").val();
 	localStorage.setItem("celular", obtNum);
 }
-var comprobar = function(e){
-	var caracteres = $(e.target).val().length;
-	if (caracteres == 9){
-		$("#next").attr("href", "verify.html");
-		$("#next").click(generarCod);
+var compararCodigo = function(){
+	var obtenCod = localStorage.getItem("guardarCod");
+
+	var iniciales = "LAB";
+	var pridig = $("#dig1").val();
+	var segdig = $("#dig2").val();
+	var terdig = $("#dig3").val();
+	var juntar = pridig + segdig + terdig;
+	var codigoRec = iniciales + juntar;
+
+	if(pridig.length == 0 || segdig.length == 0 || terdig.length == 0){
+		alert("Ingrese su código porfavor");
 	}
-	else{
-		$("#next").removeAttr("href");
-		$("#next").unbind("click");
-	}
-}
-var validar = function(e){
-	var tecla = e.keyCode;
-	if((tecla >= 48 && tecla <= 57) || tecla == 8){
-		return true;
+	else if(obtenCod == codigoRec){
+		$("#sgt2").attr("href","datos.html");
 	}
 	else
-		return false;
+		alert("Código invalido");
 }
-var cargaPag = function(){
-	$("#numeros").keydown(validar);
-	// $num.keyup(comprobar);
-	$("#numeros").on("input", function(e){
-		validar(e);
-		comprobar(e);
-	});
-	copNum();
-	// $("#dig").focus();
-	// $("#dig").keyup(digCod);
-	// $("#sgt2").click(compararCodigo);
-	
+var saltaFocus = function(e){
+	if($(e.target).val().trim().length == 1){
+		$(e.target).parent().next().children().focus();
+	}
+	if (e.keyCode == 8) {
+		$(e.target).parent().prev().children().focus();
+	}
+}
+var todoBien = function(pos){
+	var lat = pos.coords.latitude;
+	var lon = pos.coords.longitude;
+	var latlon= new google.maps.LatLng(lat, lon);
+	$("#map").addClass("tamanoMapa");
 
+	var misOpciones = {
+		center:latlon, zoom: 14,
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		mapTypeControl: false,
+		navigationControlOptions: {
+			style: google.maps.NavigationControlStyle.SMALL
+		}
+	};
+
+	var mapa = new google.maps.Map(document.getElementById("map"), misOpciones);
+
+	var marcador = new google.maps.Marker({
+		pos: latlon,
+		mapa: mapa,
+		title: "Estas aqui"
+	});
 }
-$(document).ready(cargaPag);
+var hayError = function (error){
+	alert("ERROR");
+}
